e0036bafef76de02a4f928d657a2e5cfd8ba80bf
Gianpaolo Macario <gmacario@gmail.com>
2024-03-30 20:35:28 +0100

    Complete Stage 2/7: Respond to PING
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    print(f"DEBUG: server_socket={server_socket}")
+    (connection, addr) = server_socket.accept() # wait for client
+    print(f"DEBUG: After server_socket.accept()")
+    with connection:
+        connection.recv(1024)
+        connection.sendall(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
