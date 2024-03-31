27ee822d7dec8ee487b58530a1a873da849594de
Gianpaolo Macario <gmacario@gmail.com>
2024-03-31 10:54:24 +0200

    Solution to Stage 4/7: Handle concurrent clients
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,28 +1,33 @@
 """Main module"""
 
 import socket
+import threading
+
+
+def handle_client(connection):
+    print(f"DEBUG: handle_client(connection={connection})")
+    with connection:
+        while True:
+            data = connection.recv(1024)
+            print(f"DEBUG: Received data={data}")
+            if not data:
+                break   # Client has closed the connection.
+            print("DEBUG: Sending reply: PONG")
+            connection.sendall(b'+PONG\r\n')
 
 
 def main():
     """
     Main program
     """
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
+    # print("DEBUG: main() starts here")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # print(f"DEBUG: server_socket={server_socket}")
     while True:
         (connection, addr) = server_socket.accept() # wait for client
-        print(f"DEBUG: After server_socket.accept(): (connection={connection}, addr={addr})")
-        with connection:
-            while True:
-                data = connection.recv(1024)
-                print(f"DEBUG: Received data={data}")
-                if not data:
-                    break   # Client has closed the connection.
-                print("DEBUG: Sending reply: PONG")
-                connection.sendall(b'+PONG\r\n')
+        client_thread = threading.Thread(target=handle_client, args=(connection,))
+        client_thread.start()
 
 
 if __name__ == "__main__":
